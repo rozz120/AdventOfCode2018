86a2c03d869c0ab89e688e8f30a0a0159491be6f
rozz120 <32567639+rozz120@users.noreply.github.com>
2018-12-06 13:11:49 +0000

    day one commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ namespace Advent_of_code_2018
     {
         static void Main(string[] args)
         {
+
+            var dayOnePartOne = new DayOnePartOne();
+            Console.WriteLine($"Day One, Part One Result: {dayOnePartOne.additionOfNumbers()}" );
+
+            var dayOnePartTwo = new DayOnePartTwo();
+            Console.WriteLine($"Day One, Part One Result: {dayOnePartTwo.firstReccuringNumber()}" );
+
             var dayTwoPartOne = new DayTwoPartOne();
             Console.WriteLine($"Day Two, Part One Result: {dayTwoPartOne.CheckBoxIdLetterCounts()}");
 
